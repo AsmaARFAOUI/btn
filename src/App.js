dc9a6503df02a50a96389ab547725a37aa51591c
AsmaARFAOUI <109092745+AsmaARFAOUI@users.noreply.github.com>
2023-03-28 14:15:46 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="global">
+      <button className="btn">button</button>
+      <button className="btn">button</button>
+    </div>
+  );
+}
